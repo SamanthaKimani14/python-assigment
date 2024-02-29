11730b205a902bac06790b1245ebf007f2cfa595
SamanthaKimani14 <ciirukimani14@gmail.com>
2024-02-29 09:19:49 +0300

    python assignment

---
+++ DataTypes.py
@@ -0,0 +1,13 @@
+list = ['abc', 786, 2.23, 'John', 70.2]
+tinylist = [123, 'John']
+print(tinylist)
+print(list[2:3])
+print(list + tinylist)
+print(tinylist *3)
+tuple = "abcd", 768, 2.223, 'John', 70.2
+print(tuple)
+print(tuple [1:3])
+print(tuple *8)
+tinytuple = 'Wed' ,'Sun' , 33.45
+print(tuple + tinytuple)
+dict =
---
+++ user_input.py
@@ -0,0 +1,8 @@
+# ask the user for their name 
+name = input("What is your name?" )
+# ask user for their age
+age = input("How old are you?" )
+# ask user for their location
+location = input("Where do you live?" )
+# Print out a personalized message using the user's name, age, and location
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
